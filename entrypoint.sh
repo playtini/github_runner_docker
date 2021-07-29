6e55d666687c5d2c665536753bc75e893110692e
Roman Dutchak <dutchakdev@gmail.com>
2021-07-30 00:01:35 +0300

    fix(dockerfile): runner registration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
+registration_url="https://api.github.com/orgs/${GITHUB_OWNER}/actions/runners/registration-token"
 echo "Requesting registration URL at '${registration_url}'"
-payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${registration_url})
+payload=$(curl -sX POST -H "Authorization: Bearer ${GITHUB_PERSONAL_TOKEN}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 ./config.sh \
     --name $(hostname) \
